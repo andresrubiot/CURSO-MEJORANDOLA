cb8cd2dcdac214487d12b78af0b1b540a528c497
andresrubiot <andresrubiot5@gmail.com>
2014-08-19 19:28:37 -0500

    Pokemon_imagenes
    Imagenes atacando

--- pokemon/js/script.js
+++ pokemon/js/script.js
@@ -11,26 +11,26 @@ function Pokemon(n,v,a,g,i){
 
 function yoTeElijo(poke1){
 	if (poke1==0) {var poke1=new Pokemon("Pikachu",100,55,"Pika!","pikachu.png");}
-	else if (poke1==1) {var poke1=new Pokemon("Charmander",100,30,"Char!","charmander.png");}
-	else if (poke1==2) {var poke1=new Pokemon("Yiglepop",100,20,"Yiglip!","yiglepop.png");}
-	else if (poke1==3) {var poke1=new Pokemon("poke4",100,34,"Four!","poke4.png");}
-	else if (poke1==4) {var poke1=new Pokemon("poke5",100,50,"Five!","poke5.png");}
-	else if (poke1==5) {var poke1=new Pokemon("poke6",100,35,"Six!","poke6.png");}
+	else if (poke1==1) {var poke1=new Pokemon("Charmander",100,30,"Char!","charmander.jpeg");}
+	else if (poke1==2) {var poke1=new Pokemon("Giglipop",100,20,"Giglip!","giglipop.png");}
+	else if (poke1==3) {var poke1=new Pokemon("Eevee",100,34,"Four!","eevee.png");}
+	else if (poke1==4) {var poke1=new Pokemon("Bulbasaur",100,50,"Five!","bulbasaur.jpg");}
+	else if (poke1==5) {var poke1=new Pokemon("Chikorita",100,35,"Six!","chikorita.jpg");}
 	nombrePokemon1.innerText="Elegiste a "+poke1.nombre;
-	//imagenPokemon1.innerText=poke1.imagen;
+	imagenPokemon1.innerHTML='<img src="imagenes/'+poke1.imagen+'" alt="'+poke1.nombre+'" height="100" />';
 	datosPokemon1.innerText="Tiene una vida de : "+poke1.vida+", un ataque: "+poke1.ataque+" y su grito es: "+poke1.grito;
 	document.getElementById("ataque1").value=poke1.ataque;
 }
 
 function yoTeElijo2(poke2){
 	if (poke2==0) {var poke2=new Pokemon("Pikachu",100,55,"Pika!","pikachu.png");}
-	else if (poke2==1) {var poke2=new Pokemon("Charmander",100,30,"Char!","charmander.png");}
-	else if (poke2==2) {var poke2=new Pokemon("Yiglepop",100,20,"Yiglip!","yiglepop.png");}
-	else if (poke2==3) {var poke2=new Pokemon("poke4",100,34,"Four!","poke4.png");}
-	else if (poke2==4) {var poke2=new Pokemon("poke5",100,50,"Five!","poke5.png");}
-	else if (poke2==5) {var poke2=new Pokemon("poke6",100,35,"Six!","poke6.png");}
+	else if (poke2==1) {var poke2=new Pokemon("Charmander",100,30,"Char!","charmander.jpeg");}
+	else if (poke2==2) {var poke2=new Pokemon("Giglipop",100,20,"Giglip!","giglipop.png");}
+	else if (poke2==3) {var poke2=new Pokemon("Eevee",100,34,"Four!","eevee.png");}
+	else if (poke2==4) {var poke2=new Pokemon("Bulbasaur",100,50,"Five!","bulbasaur.jpg");}
+	else if (poke2==5) {var poke2=new Pokemon("Chikorita",100,35,"Six!","chikorita.jpg");}
 	nombrePokemon2.innerText="Elegiste a "+poke2.nombre;
-	//imagenPokemon2.innerText=poke2.imagen;
+	imagenPokemon2.innerHTML='<img src="imagenes/'+poke2.imagen+'" alt="'+poke2.nombre+'" height="100" />';
 	datosPokemon2.innerText="Tiene una vida de : "+poke2.vida+", un ataque: "+poke2.ataque+" y su grito es: "+poke2.grito;
 
 	document.getElementById("ataque2").value=poke2.ataque;
